128d3b4d1c33909060d77ff865dba3dac820f774
Hoe Hao Cheng <haochengho12907@gmail.com>
2026-09-13 19:04:55 +0800

    修正传统构词法双字词编码生成

--- src/main.rs
+++ src/main.rs
@@ -322,8 +322,13 @@ fn main() {
                                     String::from(&zi2_a[0..])
                                 }
                             } else {
-                                // 首根大码与首根声码
-                                String::from(&zi2_a[0..2])
+                                if zi2_b.len() >= 2 {
+                                    // 首根大码、首根声码、次根大码
+                                    String::from(&zi2_a[0..2]) + &zi2_b[0..1]
+                                } else {
+                                    // 首根大码、首根声码、首根韵码
+                                    String::from(&zi2_a[0..3])
+                                }
                             }
                         }
                         // 首字不是小字根，次字只取二码
